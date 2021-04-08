c483d378b1b532f66f9c17be232ecacf992acdee
Ahmed Nabil <anabil@agiletz.com>
2021-04-09 01:39:33 +0200

    localization

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {StatusBar, useColorScheme} from 'react-native';
 import {Provider} from 'react-redux';
 import {Container} from './navigation';
 import {store} from './store';
+import {setI18nConfig} from './utils/i18n-helper';
 
 const App = () => {
   const theme = useColorScheme();
+
+  useEffect(() => {
+    setI18nConfig();
+  }, []);
 
   return (
     <Provider store={store}>
--- src/navigation/tab-navigator.tsx
+++ src/navigation/tab-navigator.tsx
@@ -3,6 +3,7 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {News} from '../components/pages/News';
 import {ROUTES} from '../constants/routes';
 import {Settings} from '../components/pages/Settings';
+import {translate} from '../utils/i18n-helper';
 
 type ParamList = {
   News: undefined;
@@ -14,8 +15,16 @@ const Tab = createBottomTabNavigator<ParamList>();
 export const Tabs = () => {
   return (
     <Tab.Navigator>
-      <Tab.Screen name={ROUTES.NEWS} component={News} />
-      <Tab.Screen name={ROUTES.SETTINGS} component={Settings} />
+      <Tab.Screen
+        options={{tabBarLabel: translate('News')}}
+        name={ROUTES.NEWS}
+        component={News}
+      />
+      <Tab.Screen
+        options={{tabBarLabel: translate('Settings')}}
+        name={ROUTES.SETTINGS}
+        component={Settings}
+      />
     </Tab.Navigator>
   );
 };
