20affd6742564507ecaff7f9fe54c20eb8f1de17
Ahmed Nabil <anabil@agiletz.com>
2021-04-09 22:26:03 +0200

    feat (article details) : open article details

--- src/components/organisms/ArticleItem/index.tsx
+++ src/components/organisms/ArticleItem/index.tsx
@@ -1,23 +1,25 @@
-import {useTheme} from '@react-navigation/native';
+import {useNavigation, useTheme} from '@react-navigation/native';
 import React from 'react';
 import {TouchableOpacity, View} from 'react-native';
+import {ROUTES} from '../../../constants/routes';
 import {Article} from '../../../interfaces/Article';
 import {Image} from '../../atoms/Image';
 import {Text} from '../../atoms/Text';
 import styles from './styles';
 
-export const ArticleItem: React.FC<Article> = React.memo(
-  ({urlToImage, author, title}) => {
-    const {colors} = useTheme();
-    return (
-      <TouchableOpacity
-        style={[styles.articleContainer, {borderBottomColor: colors.border}]}>
-        <Image style={styles.articleImage} source={{uri: urlToImage}} />
-        <View style={styles.descriptionContainer}>
-          <Text style={[styles.text, styles.title]}> {author}</Text>
-          <Text style={[styles.text, styles.desc]}> {title}</Text>
-        </View>
-      </TouchableOpacity>
-    );
-  },
-);
+export const ArticleItem: React.FC<Article> = React.memo(article => {
+  const {urlToImage, author, title} = article;
+  const {colors} = useTheme();
+  const {navigate} = useNavigation();
+  return (
+    <TouchableOpacity
+      onPress={() => navigate(ROUTES.DETAILS, article)}
+      style={[styles.articleContainer, {borderBottomColor: colors.border}]}>
+      <Image style={styles.articleImage} source={{uri: urlToImage}} />
+      <View style={styles.descriptionContainer}>
+        <Text style={[styles.text, styles.title]}> {author}</Text>
+        <Text style={[styles.text, styles.desc]}> {title}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+});
